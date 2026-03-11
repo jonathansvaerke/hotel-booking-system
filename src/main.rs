146878c60081db6105d709312779a1532d07236f
jonathansvaerke <jonathansvaerke@proton.me>
2026-03-11 21:50:43 +0100

    Add a couple of future functions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,52 @@
 use rusqlite::{Connection, Result};
 
-fn main() {
-    let conn = Connection::open("cats.db");
+fn main() -> Result<()> {
+    init_database()?;
+    Ok(())
+}
+
+fn add_room() {}
+
+fn add_guest() {}
+
+fn init_database() -> Result<()> {
+    let conn = Connection::open("hotel-database.db")?;
+    conn.execute("PRAGMA foreign_keys = ON;", [])?;
+    /*
+        rooms(PK, number, class, capacity)
+        guests(PK, name, email, phone)
+        booking(PK, start_date, end_date)
+    */
+    conn.execute_batch(
+        "
+        CREATE TABLE IF NOT EXISTS rooms (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            number INTEGER NOT NULL UNIQUE,
+            class INTEGER NOT NULL,
+            capacity INTEGER NOT NULL,
+            price REAL NOT NULL
+        );
+
+        CREATE TABLE IF NOT EXISTS guests (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            first_name TEXT NOT NULL,
+            last_name TEXT NOT NULL,
+            email TEXT NOT NULL UNIQUE,
+            phone Text NOT NULL UNIQUE
+        );
+
+        CREATE TABLE IF NOT EXISTS bookings (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            room_id INTEGER NOT NULL,
+            guest_id INTEGER NOT NULL,
+            start_date TEXT NOT NULL,
+            end_date TEXT NOT NULL,
+            CHECK (end_date > start_date),
+            FOREIGN KEY (room_id) REFERENCES rooms(id),
+            FOREIGN KEY (guest_id) REFERENCES guests(id)
+        );
+        ",
+    )?;
+
+    Ok(())
 }
